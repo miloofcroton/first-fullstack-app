2a7ffa4e4556633553d931cc6981c09fa7fbbd94
Jack <ajackofsometrades@gmail.com>
2018-08-29 16:10:26 -0700

    added route for structure detail to display

--- app/src/router.js
+++ app/src/router.js
@@ -1,11 +1,19 @@
 import VueRouter from 'vue-router';
 import Home from './components/home/Home.vue';
 import Structures from './components/structures/Structures.vue';
+import StructureDetail from './components/structure/StructureDetail.vue';
 
 export default new VueRouter({
   routes: [
-    { path: '/', component: Home },
-    { path: '/structures', component: Structures },
-    { path: '*', redirect: '/' }
+    { path: '/', 
+      component: Home },
+    { path: '/structures', 
+      component: Structures },
+    {
+      path: '/structure/:key',
+      component: StructureDetail,
+    },
+    { path: '*', 
+      redirect: '/' },
   ]
 });
