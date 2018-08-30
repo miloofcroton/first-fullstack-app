eca8fae5130d855ea33d382d482a1dbbdc5f64b8
Jack <ajackofsometrades@gmail.com>
2018-08-30 16:26:20 -0700

    adding a second table to create

--- server/scripts/create-tables.js
+++ server/scripts/create-tables.js
@@ -1,25 +1,22 @@
+const client = require('../db-client');
 
-//postgres user info:
-// username = postgres, password = password
+client.query(`
+    CREATE TABLE IF NOT EXISTS structures (
+      id SERIAL PRIMARY KEY,
+      name VARCHAR(256) NOT NULL,
+      color VARCHAR(256),
+      type VARCHAR(256),
+      ordered BOOLEAN,
+      elements INTEGER
+    );
 
-const pg = require('pg');
-const Client = pg.Client;
-const databaseUrl = 'postgres://postgres:password@localhost:5432/mathology';
-const client = new Client(databaseUrl);
-
-client.connect()
-  .then(() => {
-    return client.query(`
-      CREATE TABLE IF NOT EXISTS structures (
-        id SERIAL PRIMARY KEY,
-        name VARCHAR(256) NOT NULL,
-        color VARCHAR(256),
-        type VARCHAR(256),
-        ordered BOOLEAN,
-        elements INTEGER
-      );
-    `);
-  })
+    CREATE TABLE IF NOT EXISTS elements (
+      id SERIAL PRIMARY KEY,
+      name VARCHAR(256) NOT NULL,
+      parent_id INTEGER NOT NULL REFERENCES structures(id),
+      color VARCHAR(256)
+    );
+  `)
   .then(
     () => console.log('create tables complete'),
     err => console.log(err)
