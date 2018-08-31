6bdd2b45d9bfce2d5cd95e4328a1840f189773aa
Jack <ajackofsometrades@gmail.com>
2018-08-30 17:17:43 -0700

    updated flavor text :)

--- app/src/components/home/Home.vue
+++ app/src/components/home/Home.vue
@@ -1,15 +1,59 @@
 <template>
   <main>
-    <p>I'm home!</p>
+    <h2>Do you believe in math?</h2>
+    <p class="center">
+      A = math <br>
+      B = magic <br>
+      You believe in magic <br>
+      All math is magic <br>
+      You believe in magic <br>
+    </p>
+    <p>
+      Math is a funny thing. I think many people tend to see it as magic and never attempt to demystify it, even those who use math on a daily basis. My aim is to do exactly that. Let's take a harder look at the real cause and effect that makes up, well, cause and effect. (Arguing for particular math foundations quickly becomes a recursive one, as you can see).
+    </p>
+    <p>
+      This app is designed to allow you to play around with real math structures and see what happens when they combine to form larger structures. What might you stumble upon?
+    </p>
+    <p class="center">
+      Abracadabra...
+    </p>
   </main>
 </template>
 
 <script>
+
+
+
 export default {
+
+  data() {
+    return {
+      proof: `
+        
+      `
+    };
+  }
+
 
 };
 </script>
 
 <style scoped>
+
+h2 {
+  font-size: 36px;
+}
+
+p.center {
+  text-align: center;
+}
+
+p {
+  font-size: 18px;
+  text-align: left;
+  width: 30%;
+  padding: 10px;
+  margin: 10px auto;
+}
 
 </style>
